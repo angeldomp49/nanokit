3df135af386dbf1cdee6d19c3b21a467a8fc89f7
Angel Domínguez Plata <angeldomp49@gmail.com>
2021-04-20 16:00:11 -0500

    using classes

--- vendor/makechtec/nanokit/Mail/using.php
+++ vendor/makechtec/nanokit/Mail/using.php
@@ -1,11 +1,26 @@
 <?php
 
+$templateData = [
+    'name' => 'Foo',
+    'email' => 'bar'
+];
+
 $structure = [
     'host' => "mail.pixelsiete.com",
     'userName' => 'wordpress@pixelsiete.com',
     'password' => 'example',
     'fromName' => 'Emitter',
-    'subject' => 'Contact Form'
+    'subject' => 'Contact Form',
+    'addresses' => [
+        'wordpress@pixelsiete.com'
+    ],
+    'bccs' => [
+
+    ],
+    'ccs' => [
+
+    ],
+    'template' => new Template('mail.php', $templateData )
 ];
 
 $sender = new Sender( new Order( $structure ) );
